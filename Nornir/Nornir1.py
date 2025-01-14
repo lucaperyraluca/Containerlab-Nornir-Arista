36f524f6f0e72e528aebd4466e39d2dfac87b48d
lucaperyraluca <69149459+lucaperyraluca@users.noreply.github.com>
2025-01-14 09:05:03 -0300

    Update Nornir1.py

--- Nornir/Nornir1.py
+++ Nornir/Nornir1.py
@@ -5,13 +5,8 @@ from nornir_utils.plugins.functions import print_result
 #nr = InitNornir(config_file="config.yaml")
 
 def main():
-    # Inicializa Nornir con la configuración
     nr = InitNornir(config_file="config.yaml")
-    
-    # Ejecuta un comando en todos los dispositivos usando Scrapli
     result = nr.run(task=send_command, command="show version")
-    
-    # Imprime los resultados
     print_result(result)
 
 if __name__ == "__main__":
